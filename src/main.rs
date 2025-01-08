eac0a179c03467b4f66c575ecdc54fe0e67bad27
SauersML <123714540+SauersML@users.noreply.github.com>
2025-01-08 00:53:33 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -738,10 +738,10 @@ impl State {
         // WGSL
         let shader_src = r#"
 struct Globals {
-    viewProj: mat4x4<f32>;
+    viewProj: mat4x4<f32>,
 };
 struct Sun {
-    sunPos: vec4<f32>;
+    sunPos: vec4<f32>,
 };
 
 @group(0) @binding(0) var<uniform> globals: Globals;
